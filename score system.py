2a5286ebe7549abeb3b831a28ccb398b64523662
eMobilis <eunycewang@gmail.com>
2023-06-27 09:06:07 +0300

    simple score system

---
+++ score system.py
@@ -0,0 +1,21 @@
+marks = int(input("Enter your max"))
+
+if marks < 40:
+    print("E")
+
+elif marks < 50:
+    print("D")
+
+elif marks < 60:
+    print("C")
+
+elif marks < 70:
+    print("B")
+
+else:
+    print("A")
+
+
+
+
+
