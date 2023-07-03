ecada1185103f0f1e631376ee7775be855fb4c2d
eMobilis <eunycewang@gmail.com>
2023-07-03 08:46:51 +0300

    forloop

--- Loops/forloops2.py
+++ Loops/forloops2.py
@@ -1,3 +1,8 @@
 friends_age = {"roy":29, "jayden":20, "marie":25 }
 for i,n in friends_age.items():
     print(i,n)
+
+  #or
+#friends_age = {"roy":29, "jayden":20, "marie":25 }
+#for name, age in  friends_age.items():
+    #print(f"{name} : {age}")
