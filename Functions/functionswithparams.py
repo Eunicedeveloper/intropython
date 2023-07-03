8504d9a0ded81434e8edb17c60eed49be50f903f
eMobilis <eunycewang@gmail.com>
2023-07-03 10:25:59 +0300

    functionsparemeters

---
+++ Functions/functionswithparams.py
@@ -0,0 +1,10 @@
+def addnums(x,y):
+    results = x + y
+    print(results)
+
+addnums(150, 30)
+
+
+def showname(first, last):
+    print(first,last)
+showname(input("enter first name"), input("enter second name"))
