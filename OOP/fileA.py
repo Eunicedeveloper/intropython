d928214d352108b7643b80329c0992330134220b
eMobilis <eunycewang@gmail.com>
2023-07-06 09:04:32 +0300

    file

---
+++ OOP/fileA.py
@@ -0,0 +1,11 @@
+class Parentclass:
+    def parentClassMethod(self):
+        print("I'm the parent and i've been inherited")
+
+    def addnums(self, x,y):
+        result = x + y
+        print(result)
+
+    def dividenums(self,x,y ):
+        result = x/y
+        print(result)
---
+++ OOP/fileB.py
@@ -0,0 +1,8 @@
+from fileA import Parentclass
+
+obj = Parentclass()
+obj.parentClassMethod
+
+obj.addnums(10,40)
+
+obj.dividenums(20,4)
